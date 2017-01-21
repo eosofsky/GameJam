227c8821f48cce1b39a3aa7415ecf00981efac9e
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-01-20 23:05:49 -0500

    Basic player (sphere) moves around constrained space.

---
+++ GameJam/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerScript : MonoBehaviour {
+	public float speed;
+
+	private CharacterController controller;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		Move ();
+	}
+
+	void Move () {
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
