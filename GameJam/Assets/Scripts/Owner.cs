c8c189ec1b8e0055120459e551bdb9389cd6ec2d
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-01-21 15:53:42 -0500

    .

---
+++ GameJam/Assets/Scripts/Owner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Owner : MonoBehaviour {
+	GameObject stick;
+	Stick stickScript;
+	Animator animator;
+
+	void Start () {
+		stick = GameObject.FindGameObjectWithTag ("Stick");
+		stickScript = stick.GetComponent <Stick> ();
+		animator = GetComponent <Animator> ();
+
+
+	}
+}
